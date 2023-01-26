4a222dd255b93ac375f94a4a8bb7bb773e416b66
lucas.honda <contatostts@gmail.com>
2023-01-26 10:22:23 -0300

    _ primeiro comit

---
+++ app/src/main/java/visaogrupo/com/br/modulo_visitacao/MainActivity.java
@@ -0,0 +1,14 @@
+package visaogrupo.com.br.modulo_visitacao;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/visaogrupo/com/br/modulo_visitacao/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package visaogrupo.com.br.modulo_visitacao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
